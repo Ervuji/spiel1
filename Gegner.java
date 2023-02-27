2faf26fa5e12a9842e8a731569ce1660a4b9ebe8
Ervuji <126450904+Ervuji@users.noreply.github.com>
2023-02-27 09:43:11 +0100

    Create Gegner.java

---
+++ Gegner.java
@@ -0,0 +1,15 @@
+public class Gegner {
+    String name;
+    int lebenspunkte;
+    int angriffswert;
+    int Stamina;
+
+    public Gegner(String name, int lebenspunkte, int angriffswert, int Stamina) {
+        this.name = name;
+        this.lebenspunkte = lebenspunkte;
+        this.angriffswert = angriffswert;
+        this.Stamina = Stamina;
+    }
+
+    
+}
