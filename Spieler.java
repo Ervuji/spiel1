b093de6ec6dd10667cf1b1bf256ab4544de3bf52
Ervuji <126450904+Ervuji@users.noreply.github.com>
2023-02-27 09:45:51 +0100

    Create Spieler.java

---
+++ Spieler.java
@@ -0,0 +1,14 @@
+public class Spieler {
+    String name;
+    int lebenspunkte;
+    int angriffswert;
+    int Stamina;
+    
+    public Spieler(String name, int lebenspunkte, int angriffswert, int Stamina) {
+        this.name = name;
+        this.lebenspunkte = lebenspunkte;
+        this.angriffswert = angriffswert;
+        this.Stamina = Stamina;
+    }
+
+}
